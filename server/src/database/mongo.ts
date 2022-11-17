1e318da12ec11eff4553de8dcd3ed685cc5947ff
lamongabriel <gabriel-lamon@outlook.com>
2022-11-17 09:06:45 -0300

    api basic setup, typescript express, mongodb

---
+++ server/src/database/mongo.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const url = process.env.MONGO_DB_URL
+
+export async function connectToMongo() {
+	if(url){
+		await mongoose.connect(url)
+		return console.log('⚡ Connected to mongo!')
+	}
+	else {
+		throw new Error('😫 Missing .env configuration')
+	}
+}
---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectToMongo } from './database/mongo'
+
+dotenv.config()
+
+const server = async () => {
+	await connectToMongo()
+
+	const app = express()
+
+	app.listen(process.env.PORT, () => {
+		console.log(`🚀 Server is running on http://localhost:${process.env.PORT}`)
+	})
+}
+
+server()
